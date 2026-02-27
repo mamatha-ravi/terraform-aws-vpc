06f5682082e95fe326f857f507de2a88aebdb3bc
mamatha-ravi <mamata.r@gmail.com>
2026-02-27 17:29:21 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -12,4 +12,11 @@ vpc_final_tags = merge(
     },
    var.vpc_tags 
 )
+igw_final_tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    },
+    var.igw_tags
+)
 }
